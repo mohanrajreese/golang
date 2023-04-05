21f363c550287b0e511e3881e8dcb475bfe53dd5
Mohanraj <mohanraj@loanwiser.in>
2023-04-06 00:39:55 +0530

    values.go file added and done some practices on strings, integers, floats, booleans

---
+++ golang_practice/values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("My name is : ", "Mohan"+"raj M")
+	fmt.Println("Sum of two numbers 17 + 18 is: ", 17+18)
+	fmt.Println("15.0/3.0 is : ", 15.0/3.0)
+	fmt.Println("14.0/3.0 is : ", 14.0/3.0)
+	fmt.Println("true && false is : ", true && false)
+	fmt.Println("true || false is : ", true || false)
+	fmt.Println("!true is : ", !true)
+
+}
